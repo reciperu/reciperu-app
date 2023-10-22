943a1a9e5b167b4ff9f7d22f4c453ab2a5fb2e0b
RyoTa <ryotanny@gmail.com>
2023-10-23 02:52:28 +0900

    feat: Add fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,35 @@
+import { useFonts } from 'expo-font';
 import { Slot } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import { View } from 'react-native';
+
+import { AuthProvider } from '@/context/authProvider';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Slot />;
+  const [fontsLoaded] = useFonts({
+    'LINE-bold': require('assets/fonts/LINESeedJP_A_OTF_Bd.otf'),
+    'LINE-extra-bold': require('assets/fonts/LINESeedJP_A_OTF_Eb.otf'),
+    'LINE-regular': require('assets/fonts/LINESeedJP_A_OTF_Rg.otf'),
+    'LINE-light': require('assets/fonts/LINESeedJP_A_OTF_Th.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <AuthProvider>
+      <View onLayout={onLayoutRootView}>
+        <Slot />
+      </View>
+    </AuthProvider>
+  );
 }
