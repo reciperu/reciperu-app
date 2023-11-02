e687e0facb24c991022e7d4ddce3c2eea3dc5a9b
RyoTa <ryotanny@gmail.com>
2023-11-02 13:18:07 +0900

    feat: Add constant

---
+++ src/constants/api.ts
@@ -0,0 +1,5 @@
+// api
+export const API_TIMEOUT = 1000 * 60 * 60 * 24 * 365 * 10; // 10years
+// error message
+export const AUTH_ERROR_MESSAGE = 'ログインが必要です';
+export const UNEXPECTED_ERROR_MESSAGE = '予期せぬエラーが発生しました';
