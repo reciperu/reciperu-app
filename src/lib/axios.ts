a8daf4f39d5e8826539a597fa9e551d2581b99ce
RyoTa <ryotanny@gmail.com>
2023-11-02 13:18:22 +0900

    feat: Add axios lib

---
+++ src/lib/axios.ts
@@ -0,0 +1,57 @@
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import Axios from 'axios';
+import { router } from 'expo-router';
+import qs, { parse } from 'qs';
+
+import { API_TIMEOUT, AUTH_ERROR_MESSAGE } from '@/constants';
+import secureStoreService, { StoreKeyEnum } from '@/lib/secureStore';
+
+const config = {
+  timeout: API_TIMEOUT,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  paramsSerializer: {
+    encode: parse as any,
+    serialize: (params: Record<string, any>) => qs.stringify(params, { arrayFormat: 'brackets' }),
+  },
+  retry: false,
+};
+
+const client = Axios.create(config);
+
+client.interceptors.request.use(
+  async (config) => {
+    if (!config.headers['token']) {
+      const token = await secureStoreService.getValueFor(StoreKeyEnum.TOKEN);
+      if (!token) {
+        // note: 今のところ認証必須
+        router.push('(auth)/sign-in');
+        return Promise.reject(AUTH_ERROR_MESSAGE);
+      } else {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response?.status === 401) {
+      // リトライ処理
+      if (!error.config.retry) {
+        // tokenの更新
+        const res = await GoogleSignin.getTokens();
+        await secureStoreService.save(StoreKeyEnum.TOKEN, res.idToken);
+        error.config.retry = true;
+        return client.request(error.config);
+      }
+      return Promise.reject(error);
+    }
+  }
+);
+
+export { client };
