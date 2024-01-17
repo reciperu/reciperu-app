93a1b2314847f5190a787e5522e925314f407254
Matsumoto Ryota <awpjdmga1943@icloud.com>
2024-01-17 11:43:34 +0900

    feat: update app.config

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,32 @@
+import 'dotenv/config';
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => {
   return {
     ...config,
-    name: config.name || 'レシピル',
-    slug: config.slug || 'reciperu-app',
+    name: process.env.EXPO_PUBLIC_APP_NAME || 'レシピル',
+    slug: process.env.EXPO_PUBLIC_SCHEME || 'reciperu-app',
+    version: '1.0.0',
+    orientation: 'portrait',
     icon:
       process.env.NODE_ENV === 'development' ? './assets/dev/icon.png' : './assets/prd/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: false,
+      // 環境変数からバンドル識別子を取得
+      bundleIdentifier: process.env.IOS_BUNDLE_IDENTIFIER || 'com.ryotanny.reciperu-app-develop',
+      googleServicesFile: './GoogleService-Info.plist',
+      infoPlist: {
+        CFBundleLocalizations: ['ja_JP'],
+        CFBundleDevelopmentRegion: 'ja_JP',
+      },
+    },
     android: {
       ...config.android,
       adaptiveIcon: {
@@ -16,6 +36,29 @@ export default ({ config }: ConfigContext): ExpoConfig => {
             ? './assets/dev/adaptive-icon.png'
             : './assets/prd/adaptive-icon.png',
       },
+      googleServicesFile: './google-services.json',
+      package: process.env.ANDROID_PACKAGE || 'com.ryotanny.reciperu-dev',
     },
+    plugins: [
+      'expo-router',
+      '@react-native-google-signin/google-signin',
+      [
+        'expo-image-picker',
+        {
+          photosPermission: '$(PRODUCT_NAME)があなたの写真にアクセスすることを許可してください。',
+          cameraPermission: '$(PRODUCT_NAME)がカメラにアクセスすることを許可してください。',
+        },
+      ],
+      [
+        'expo-barcode-scanner',
+        {
+          cameraPermission: '$(PRODUCT_NAME)がカメラにアクセスすることを許可してください。',
+        },
+      ],
+    ],
+    experiments: {
+      tsconfigPaths: true,
+    },
+    scheme: process.env.EXPO_PUBLIC_SCHEME,
   };
 };
