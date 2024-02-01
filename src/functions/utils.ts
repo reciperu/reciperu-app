61518f79807e2b6b984365b4b3ef2317432186d6
Matsumoto Ryota <awpjdmga1943@icloud.com>
2024-02-01 22:31:57 +0900

    feat: Add utils

---
+++ src/functions/utils.ts
@@ -0,0 +1,11 @@
+import * as Linking from 'expo-linking';
+
+export const noop = () => {};
+
+export const getRandomNumber = (count: number) => {
+  return Math.floor(Math.random() * count);
+};
+
+export const openURL = (url: string) => {
+  Linking.openURL(url);
+};
