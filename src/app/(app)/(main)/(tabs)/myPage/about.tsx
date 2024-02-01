85e3cbaca7d28a9b3234e742390bebcb9a0bf88c
Matsumoto Ryota <awpjdmga1943@icloud.com>
2024-02-02 00:02:07 +0900

    feat: Update about page

--- src/app/(app)/(main)/(tabs)/myPage/about.tsx
+++ src/app/(app)/(main)/(tabs)/myPage/about.tsx
@@ -1,11 +1,24 @@
 import { Container } from '@/components/ui/Container';
+import { Constants } from '@/constants';
 import { APP_NAME } from '@/features/Onboarding/Recipe/constants';
+import * as ExpoConstants from 'expo-constants';
 import { Text } from 'react-native';
 
 export default function AboutPage() {
+  // アプリのバージョンを取得
+  const appVersion = (ExpoConstants.default as any).manifest.version;
   return (
-    <Container>
+    <Container bgColor={Constants.colors.primitive.gray[100]}>
       <Text>{APP_NAME}について</Text>
+      <Text
+        style={{
+          color: Constants.colors.primitive.gray[400],
+          textAlign: 'center',
+          marginTop: 20,
+          marginBottom: 40,
+        }}>
+        v{appVersion}
+      </Text>
     </Container>
   );
 }
