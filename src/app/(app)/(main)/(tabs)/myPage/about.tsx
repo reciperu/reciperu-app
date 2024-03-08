816fc583dd4f863e718cc354b9365da0a539e819
Matsumoto Ryota <awpjdmga1943@icloud.com>
2024-03-09 02:00:43 +0900

    feat: about page

--- src/app/(app)/(main)/(tabs)/myPage/about.tsx
+++ src/app/(app)/(main)/(tabs)/myPage/about.tsx
@@ -1,12 +1,29 @@
+import * as ExpoConstants from 'expo-constants';
+import { Image } from 'expo-image';
+import { useCallback, useMemo } from 'react';
+import { Pressable, ScrollView, Text, View } from 'react-native';
+
 import { Container } from '@/components/ui/Container';
 import { Flex } from '@/components/ui/Flex';
+import { Spacer } from '@/components/ui/Spacer';
 import { NotoText } from '@/components/ui/Text';
+import { AppIcon } from '@/components/ui/icons';
 import { Constants } from '@/constants';
-import { APP_NAME } from '@/features/Onboarding/Recipe/constants';
 import { useFetchRoadmap } from '@/features/Roadmap/apis/getRoadmap';
-import * as ExpoConstants from 'expo-constants';
-import { useCallback, useMemo } from 'react';
-import { Text, View } from 'react-native';
+import { openURL } from '@/functions/utils';
+
+const DEVELOPER_LIST = [
+  {
+    name: 'りょーた',
+    x: 'https://twitter.com/RyoTa___0222',
+    image: require('assets/developer_ryota.png'),
+  },
+  {
+    name: 'まーとくん',
+    x: 'https://twitter.com/tezmasatoo',
+    image: require('assets/developer_masato.png'),
+  },
+];
 
 export default function AboutPage() {
   const { data } = useFetchRoadmap();
@@ -45,72 +62,124 @@ export default function AboutPage() {
   // アプリのバージョンを取得
   const appVersion = (ExpoConstants.default as any).manifest.version;
   return (
-    <Container bgColor={Constants.colors.primitive.gray[100]}>
-      <Text>{APP_NAME}について</Text>
-      {displayData.length > 0 && (
-        <View style={{ marginTop: 24 }}>
-          <Flex style={{ alignItems: 'center', gap: 4, marginBottom: 4 }}>
-            <View
+    <ScrollView style={{ backgroundColor: Constants.colors.primitive.pink[50] }}>
+      <Container bgColor={Constants.colors.primitive.pink[50]}>
+        <Flex style={{ justifyContent: 'center', marginVertical: 24 }}>
+          <Image source={require('assets/logo-vertical.svg')} style={{ width: 84, height: 126 }} />
+        </Flex>
+        <Flex style={{ alignItems: 'center', gap: 4, marginBottom: 6 }}>
+          <View
+            style={{
+              width: 10,
+              height: 10,
+              backgroundColor: Constants.colors.primitive.pink[300],
+              borderRadius: 5,
+            }}
+          />
+          <NotoText style={{ fontSize: 12, lineHeight: 16 }}>開発者</NotoText>
+        </Flex>
+        {DEVELOPER_LIST.map((item) => (
+          <Pressable onPress={() => openURL(item.x)} key={item.name}>
+            <Flex
               style={{
-                width: 10,
-                height: 10,
-                backgroundColor: Constants.colors.primitive.pink[300],
-                borderRadius: 5,
-              }}
-            />
-            <NotoText style={{ fontSize: 12, lineHeight: 16 }}>追加予定の機能</NotoText>
-          </Flex>
-          <Flex style={{ flexDirection: 'column', gap: 12 }}>
-            {displayData.map((item) => (
-              <Flex
-                key={item.id}
-                style={{
-                  alignItems: 'center',
-                  padding: 12,
-                  gap: 8,
-                  backgroundColor: 'white',
-                  borderRadius: Constants.radius.lg,
-                }}>
-                <View style={{ flex: 1 }}>
-                  <NotoText style={{ fontSize: 14, lineHeight: 18 }}>{item.title}</NotoText>
-                  {item.description && (
-                    <NotoText style={{ fontSize: 10, color: Constants.colors.primitive.gray[600] }}>
-                      {item.description}
-                    </NotoText>
-                  )}
-                </View>
-                <Flex style={{ alignItems: 'center', gap: 4 }}>
-                  <View
-                    style={{
-                      width: 8,
-                      height: 8,
-                      backgroundColor: getStatusColor(item.status[0]),
-                      borderRadius: 4,
-                    }}
-                  />
-                  <NotoText
-                    style={{
-                      fontSize: 12,
-                      lineHeight: 16,
-                      color: Constants.colors.primitive.gray[600],
-                    }}>
-                    {getStatusLabel(item.status[0])}
-                  </NotoText>
-                </Flex>
+                alignItems: 'center',
+                gap: 12,
+                marginBottom: 16,
+                backgroundColor: 'white',
+                padding: 12,
+                borderRadius: Constants.radius.lg,
+              }}>
+              <Image source={item.image} style={{ width: 48, height: 48, borderRadius: 24 }} />
+              <Flex style={{ flexDirection: 'column', gap: 4 }}>
+                <NotoText fw="bold" style={{ fontSize: 14, lineHeight: 16 }}>
+                  {item.name}
+                </NotoText>
+                <NotoText
+                  style={{
+                    fontSize: 12,
+                    lineHeight: 14,
+                    color: Constants.colors.primitive.gray[500],
+                  }}>
+                  Xで見る
+                </NotoText>
+                <Spacer />
               </Flex>
-            ))}
-          </Flex>
-        </View>
-      )}
-      <Text
-        style={{
-          color: Constants.colors.primitive.gray[400],
-          textAlign: 'center',
-          marginTop: 20,
-          marginBottom: 40,
-        }}>
-        v{appVersion}
-      </Text>
-    </Container>
+              <Spacer />
+              <AppIcon
+                name="new-window-arrow"
+                width={20}
+                height={20}
+                color={Constants.colors.primitive.gray[300]}
+              />
+            </Flex>
+          </Pressable>
+        ))}
+        {displayData.length > 0 && (
+          <View style={{ marginTop: 24 }}>
+            <Flex style={{ alignItems: 'center', gap: 4, marginBottom: 6 }}>
+              <View
+                style={{
+                  width: 10,
+                  height: 10,
+                  backgroundColor: Constants.colors.primitive.pink[300],
+                  borderRadius: 5,
+                }}
+              />
+              <NotoText style={{ fontSize: 12, lineHeight: 16 }}>追加予定の機能</NotoText>
+            </Flex>
+            <Flex style={{ flexDirection: 'column', gap: 12 }}>
+              {displayData.map((item) => (
+                <Flex
+                  key={item.id}
+                  style={{
+                    alignItems: 'center',
+                    padding: 12,
+                    gap: 8,
+                    backgroundColor: 'white',
+                    borderRadius: Constants.radius.lg,
+                  }}>
+                  <View style={{ flex: 1 }}>
+                    <NotoText style={{ fontSize: 14, lineHeight: 18 }}>{item.title}</NotoText>
+                    {item.description && (
+                      <NotoText
+                        style={{ fontSize: 10, color: Constants.colors.primitive.gray[600] }}>
+                        {item.description}
+                      </NotoText>
+                    )}
+                  </View>
+                  <Flex style={{ alignItems: 'center', gap: 4 }}>
+                    <View
+                      style={{
+                        width: 8,
+                        height: 8,
+                        backgroundColor: getStatusColor(item.status[0]),
+                        borderRadius: 4,
+                      }}
+                    />
+                    <NotoText
+                      style={{
+                        fontSize: 12,
+                        lineHeight: 16,
+                        color: Constants.colors.primitive.gray[600],
+                      }}>
+                      {getStatusLabel(item.status[0])}
+                    </NotoText>
+                  </Flex>
+                </Flex>
+              ))}
+            </Flex>
+          </View>
+        )}
+        <Text
+          style={{
+            color: Constants.colors.primitive.gray[400],
+            textAlign: 'center',
+            marginTop: 20,
+            marginBottom: 40,
+          }}>
+          v{appVersion}
+        </Text>
+      </Container>
+    </ScrollView>
   );
 }
