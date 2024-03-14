721cab24db48c4f0a7b6b9cd7ce14e00c6748695
Matsumoto Ryota <awpjdmga1943@icloud.com>
2024-03-14 16:17:11 +0900

    feat: Add RoadmapItem component

---
+++ src/features/Roadmap/components/RoadmapItem.tsx
@@ -0,0 +1,71 @@
+import { memo, useCallback } from 'react';
+import { View } from 'react-native';
+
+import { Constants } from '@/constants';
+import { RoadmapItem as Props } from '@/features/Roadmap/types';
+import { Flex } from '@/features/chore/Flex';
+import { NotoText } from '@/features/chore/Text';
+
+export const RoadmapItem = memo<Props>((item) => {
+  const getStatusLabel = useCallback((status: '対応中' | '未対応' | '対応済') => {
+    if (status === '対応中') {
+      return '開発中';
+    }
+    if (status === '未対応') {
+      return '未対応';
+    }
+    if (status === '対応済') {
+      return '対応済';
+    }
+  }, []);
+  const getStatusColor = useCallback((status: '対応中' | '未対応' | '対応済') => {
+    if (status === '対応中') {
+      return '#48BB78';
+    }
+    if (status === '未対応') {
+      return '#ECC94B';
+    }
+    if (status === '対応済') {
+      return '#90CDF4';
+    }
+  }, []);
+  return (
+    <Flex
+      key={item.id}
+      style={{
+        alignItems: 'center',
+        padding: 12,
+        gap: 12,
+        backgroundColor: 'white',
+        borderRadius: Constants.radius.lg,
+      }}>
+      <View style={{ flex: 1 }}>
+        <NotoText style={{ fontSize: 14, lineHeight: 18 }}>{item.title}</NotoText>
+        {item.description && (
+          <NotoText
+            style={{ fontSize: 10, color: Constants.colors.primitive.gray[700], marginTop: 4 }}>
+            {item.description}
+          </NotoText>
+        )}
+      </View>
+      <Flex style={{ alignItems: 'center', gap: 4 }}>
+        <View
+          style={{
+            width: 8,
+            height: 8,
+            backgroundColor: getStatusColor(item.status[0]),
+            borderRadius: 4,
+          }}
+        />
+        <NotoText
+          style={{
+            fontSize: 12,
+            lineHeight: 16,
+            color: Constants.colors.primitive.gray[600],
+          }}>
+          {getStatusLabel(item.status[0])}
+        </NotoText>
+      </Flex>
+    </Flex>
+  );
+});
