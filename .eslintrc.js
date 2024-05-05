74de8d8f6bff260d05199a194bc0eb0b5f3be20d
Matsumoto Ryota <awpjdmga1943@icloud.com>
2024-05-05 20:41:43 +0900

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,11 @@ module.exports = {
   root: true,
   extends: [
     'universe/native',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/display-name': 'off',
     'react/prop-types': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
