354a1f586598292f43913a1ea928c316bd71d244
RyoTa <ryotanny@gmail.com>
2024-06-23 23:17:55 +0900

    fix fetchMetaData func

--- src/features/Recipe/apis/getMetaData.ts
+++ src/features/Recipe/apis/getMetaData.ts
@@ -4,12 +4,13 @@ import { MetaData } from '../types';
 
 import { client } from '@/lib/axios';
 
-export const fetchMetaData = async (url: string): Promise<MetaData> => {
+export const fetchMetaData = async (url: string): Promise<MetaData | null> => {
+  if (url.length === 0) return null;
   return await client.get(`/recipes/meta-data?recipeUrl=${url}`);
 };
 
 type UseFetchMetaData = {
-  config?: UseMutationOptions<MetaData, unknown, string, unknown>;
+  config?: UseMutationOptions<MetaData | null, unknown, string, unknown>;
 };
 
 export const useFetchMetaData = ({ config }: UseFetchMetaData) => {
