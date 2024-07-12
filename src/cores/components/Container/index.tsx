cab37a9646d4fb3531bbc0d0bb702f77529c2cc9
RyoTa <ryotanny@gmail.com>
2024-07-13 01:37:55 +0900

    update style of container

--- src/cores/components/Container/index.tsx
+++ src/cores/components/Container/index.tsx
@@ -1,13 +1,14 @@
 import { PropsWithChildren, memo } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
 
 interface Props {
   bgColor?: string;
   needBottomPadding?: boolean;
+  style?: StyleProp<ViewStyle>;
 }
 
 export const Container = memo<PropsWithChildren<Props>>(
-  ({ children, bgColor = '#FFFFFF', needBottomPadding = true }) => {
+  ({ children, bgColor = '#FFFFFF', needBottomPadding = true, style }) => {
     return (
       <View
         style={[
@@ -16,6 +17,7 @@ export const Container = memo<PropsWithChildren<Props>>(
             backgroundColor: bgColor,
             paddingBottom: needBottomPadding ? 50 : 0,
           },
+          style,
         ]}>
         {children}
       </View>
