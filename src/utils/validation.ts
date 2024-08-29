30c11a30be3f91a3ca95d0cfb50748be8c80ccbf
RyoTa <ryotanny@gmail.com>
2024-08-29 21:35:42 +0900

    feat: Update validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,8 @@
 export const isValidUrl = (value: string) => {
   try {
-    new URL(value);
+    // スペースがあればfalse
+    if (value.includes(' ')) return false;
+    const _ = new URL(value);
     return true;
   } catch (_) {
     return false;
