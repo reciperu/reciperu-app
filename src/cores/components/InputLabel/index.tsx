5dad5ecf7b0cd6a08d9158afd08cf78b794fd3b9
RyoTa <ryotanny@gmail.com>
2024-08-29 21:37:46 +0900

    feat: Update input label

--- src/cores/components/InputLabel/index.tsx
+++ src/cores/components/InputLabel/index.tsx
@@ -1,7 +1,8 @@
 import { PropsWithChildren, memo } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import { NotoText } from '../Text';
+import { Constants } from '@/constants';
 
 interface Props {
   required?: boolean;
@@ -9,16 +10,40 @@ interface Props {
 
 export const InputLabel = memo<PropsWithChildren<Props>>(({ children, required = false }) => {
   return (
-    <NotoText style={styles.wrapper}>
-      {children}
-      {required && '（必須）'}
-    </NotoText>
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 4,
+      }}>
+      <NotoText style={styles.wrapper}>{children}</NotoText>
+      {required && (
+        <View
+          style={{
+            backgroundColor: Constants.colors.primitive.red[50],
+            marginLeft: 8,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: Constants.radius.full,
+            paddingHorizontal: 8,
+            paddingVertical: 4,
+          }}>
+          <NotoText
+            fw="bold"
+            style={{
+              fontSize: 10,
+              color: Constants.colors.primitive.red[500],
+            }}>
+            必須
+          </NotoText>
+        </View>
+      )}
+    </View>
   );
 });
 
 const styles = StyleSheet.create({
   wrapper: {
     fontSize: 12,
-    marginBottom: 4,
   },
 });
